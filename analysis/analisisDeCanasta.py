c533f5b483da78eec7a237a40ad890929e3b853f
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-01-23 20:48:47 -0500

    nt3 se crea ejemplo de analisis con pandas

---
+++ analysis/analisisDeCanasta.py
@@ -0,0 +1,17 @@
+#1. Importar el pquete o paquetes con los que voy a analizar la información.
+
+import pandas as pd
+
+
+def analizarCanastaBasica():
+    #2. Sin importar la fuente (sql, xls, JSON, scv...)
+    #Crear una tabla tabulada que se llama DATAFRAME
+    #Columnas= Atribrutos...    Filas= Valores...
+    tabla =  pd.read_csv('./data/bdcanasta.csv')
+    #print(tabla)
+
+    #3. Aplico filtros para limpiar o seleccionar datos.
+    #print(tabla.head(20)) #primeros N registros
+    #print(tabla.tail(20)) #últimos N registros
+    #print(tabla.describe) #estadistica descriptiva básica
+    
